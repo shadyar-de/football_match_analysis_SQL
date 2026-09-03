d5fbdabf6b25a16df965d1b2088d7681d3baafd5
shadyar-de <shadyar.id@gmail.com>
2026-09-03 11:37:03 +0300

    a dimentional table holding the ids, they are given from the data generator ( StatBomb) themslves, the the table can be populated. (More info will be provided in the call)

---
+++ sql/ods/match_metadata.sql
@@ -0,0 +1,23 @@
+create table if not exists dim_match (
+     match_id              VARCHAR(50)   NOT NULL,
+    competition_id        VARCHAR(50),
+    season_id             VARCHAR(50)
+                                     );
+
+alter table dim_match
+add column source_match_id varchar(50);
+
+select * from dim_match;
+
+insert into dim_match (source_match_id,match_id, competition_id, season_id)
+values
+(
+ 3825848,
+ 3825848,
+ 'LA_LIGA', --
+ '2015_2016'
+);
+--NOTE
+-- Rubén Iván Martínez Andrade wore the number
+-- 13 jersey as a goalkeeper during his stint with L
+-- evante UD, specifically in the 2015–16 La Liga season.
